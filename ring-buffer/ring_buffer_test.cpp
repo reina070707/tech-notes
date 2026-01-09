2cbe92bbd271f2c2fc063fc1893e9c8aae6161f1
Reina Hirata <reina.taniguchi.hirata@gmail.com>
2026-01-10 05:48:59 +0900

    Add tests for coverage of ringbuffer

--- ring-buffer/ring_buffer_test.cpp
+++ ring-buffer/ring_buffer_test.cpp
@@ -33,3 +33,54 @@ TEST(RingBufferTest, Overwrite) {
     EXPECT_EQ(rb.pop(), 2);
     EXPECT_EQ(rb.pop(), 3);
 }
+
+TEST(RingBufferTest, ConstructorThrow) {
+    EXPECT_THROW({
+        RingBuffer<int> rb(0);
+    }, std::invalid_argument);
+}
+
+TEST(RingBufferTest, ConstructorNoThrow) {
+    EXPECT_NO_THROW({
+        RingBuffer<int> rb(10);
+    });
+}
+
+TEST(RingBufferTest, Empty) {
+    RingBuffer<int> rb(2);
+    EXPECT_TRUE(rb.is_empty());
+    EXPECT_FALSE(rb.is_full());
+    EXPECT_EQ(rb.size(), 0);
+    EXPECT_EQ(rb.pop(), std::nullopt);
+}
+
+TEST(RingBufferTest, Senario) {
+    RingBuffer<std::string> rb(2);
+
+    rb.push("hello");
+    rb.push("robotics");
+
+    EXPECT_FALSE(rb.is_empty());
+    EXPECT_TRUE(rb.is_full());
+    EXPECT_EQ(rb.size(), 2);
+    EXPECT_EQ(rb.pop(), "hello");
+    EXPECT_EQ(rb.size(), 1);
+}
+
+TEST(RingBufferTest, CapacityOne) {
+    RingBuffer<int> rb(1);
+    rb.push(100);
+    EXPECT_EQ(rb.size(), 1);
+    rb.push(200); // Overwrite 100
+    EXPECT_EQ(rb.pop(), 200);
+    EXPECT_TRUE(rb.is_empty());
+}
+
+TEST(RingBufferTest, MultiCycle) {
+    RingBuffer<int> rb(2);
+    for(int i = 0; i < 100; ++i) {
+        rb.push(i);
+        EXPECT_EQ(rb.pop(), i);
+    }
+    EXPECT_TRUE(rb.is_empty());
+}
