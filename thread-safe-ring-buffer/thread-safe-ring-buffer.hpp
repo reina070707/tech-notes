9eb7b4406bd517e9afeb69f028d1326e434c9bf5
Reina Hirata <reina.taniguchi.hirata@gmail.com>
2026-01-12 05:37:29 +0900

    enhancement ring buffer

--- thread-safe-ring-buffer/thread-safe-ring-buffer.hpp
+++ thread-safe-ring-buffer/thread-safe-ring-buffer.hpp
@@ -1,6 +1,10 @@
+#include <bit>
+#include <stdexcept>
 #include <mutex>
 #include <condition_variable>
 #include <optional>
+#include <stop_token>
+#include <vector>
 
 template <typename T>
 class ThreadSafeRingBuffer {
@@ -12,32 +16,43 @@ private:
     size_t capacity_;
 
     std::mutex mtx_;
-    std::condition_variable cond_;
+    std::condition_variable_any cond_;
 
 public:
     explicit ThreadSafeRingBuffer(size_t cap) : capacity_(cap) {
-        buffer_.resize(cap);
+        if (capacity_ == 0) {
+            throw std::invalid_argument("Capacity must be greater than 0");
+        }
+
+        // Automatically round up 'cap' to the nearest power of 2
+        // e.g., 10 -> 16, 100 -> 128, 1024 -> 1024
+        capacity_ = std::bit_ceil(cap);
+
+        buffer_.resize(capacity_);
     }
 
     void push(T item) {
         std::lock_guard<std::mutex> lock(mtx_);
 
         buffer_[tail_] = std::move(item);
-        tail_ = (tail_ + 1) % capacity_;
+        tail_ = (tail_ + 1) & (capacity_ - 1);
         size_++;
 
         // notify
         cond_.notify_one();
     }
 
-    void wait_and_pop(T& value) {
+    std::optional<T> wait_and_pop(std::stop_token st = {}) {
         std::unique_lock<std::mutex> lock(mtx_);
 
         // waiting for a value pushed
-        cond_.wait(lock, [this] { return size_ > 0; });
+        bool success = cond_.wait(lock, st, [this] { return size_ > 0; });
 
-        value = std::move(buffer_[head_]);
-        head_ = (head_ + 1) % capacity_;
+        if (!success || st.stop_requested()) return std::nullopt;
+
+        T value = std::move(buffer_[head_]);
+        head_ = (head_ + 1) & (capacity_ - 1);
         size_--;
+        return value;
     }
 };
